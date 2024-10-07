1f9f24e0cfbabd06f747b9d4a41820be80c9d78f
dmitr <amfimitrul@gmail.com>
2024-10-07 16:52:57 +0300

    working save

--- src/main/java/spitter_maven/config/HibernateUtil.java
+++ src/main/java/spitter_maven/config/HibernateUtil.java
@@ -4,7 +4,6 @@ import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
 public class HibernateUtil {
 
     private final SessionFactory sessionFactory;
---
+++ src/main/java/spitter_maven/controller/SpitterController.java
@@ -0,0 +1,22 @@
+package spitter_maven.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class SpitterController extends HttpServlet {
+
+    // Handles GET requests (e.g., when user loads the spitter page)
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    // Handles POST requests (e.g., form submissions)
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
